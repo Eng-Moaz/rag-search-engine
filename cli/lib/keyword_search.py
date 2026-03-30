debfda17b5f573ae3c0be735d9ddd372095b64e8
Eng-Moaz <156330236+Eng-Moaz@users.noreply.github.com>
2026-03-30 09:04:34 +0200

    Naive tokenization

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,18 +1,23 @@
 from .search_utils import load_movies
 import string
 
-def preprocess(query:str):
+def tokenize(query:str):
     query = query.lower()
     removing_table = str.maketrans("","",string.punctuation)
-    return query.translate(removing_table)
+    clean_query = query.translate(removing_table)
+    tokens = clean_query.split()
+    return tokens
 
 
 def search_query(query:str,num_limit=5):
     movies = load_movies()
     result = []
+    query_tokens = tokenize(query)
     for movie in movies:
-        if preprocess(query) in preprocess(movie["title"]):
-            result.append(movie["title"])
-        if len(result) == num_limit:
-            break
+        movie_title = movie["title"]
+        tokenized_movie_title = tokenize(movie_title)
+        for query_token in query_tokens:
+            for movie_token in tokenized_movie_title:
+                if movie_title not in result and query_token in movie_token:
+                    result.append(movie_title)
     return result
