820ba196bb6994474e1c670f514d3cd00d0f684d
Eng-Moaz <156330236+Eng-Moaz@users.noreply.github.com>
2026-04-14 22:02:34 +0200

    Hybrid search setup

--- cli/lib/hybrid_search.py
+++ cli/lib/hybrid_search.py
@@ -4,6 +4,18 @@ from . import inverted_index
 from .inverted_index import InvertedIndex
 from .semantic_search import ChunkedSemanticSearch
 
+
+def normalize(scores):
+    if not scores or len(scores) == 0:
+        return
+    smallest, largest = min(scores), max(scores)
+    if smallest == largest:
+        final = [1.0] * len(scores)
+    else:
+        final = [(score - smallest) / (largest - smallest) for score in scores]
+    return final
+
+
 class HybridSearch:
     def __init__(self, documents):
         self.documents = documents
@@ -20,7 +32,23 @@ class HybridSearch:
         return self.idx.bm25_search(query, limit)
 
     def weighted_search(self, query, alpha, limit=5):
-        raise NotImplementedError("Weighted hybrid search is not implemented yet.")
+        results_bm25 = self._bm25_search(query, 500*limit)
+        results_semantic = self.semantic_search.search_chunks(query,500*limit)
+        results_combined = {}
+
+        bm25_scores = [score for doc_id, document, score in results_bm25]
+        normalized_bm25_scores = normalize(bm25_scores)
+
+        semantic_scores = [result["score"] for result in results_semantic]
+        normalized_semantic_scores = normalize(semantic_scores)
+
+        for i in range(limit * 500):
+            doc_id, document, score = results_bm25[i]
+            results_combined[doc_id] = {
+                ""
+            }
+
 
     def rrf_search(self, query, k, limit=10):
         raise NotImplementedError("RRF hybrid search is not implemented yet.")
+
