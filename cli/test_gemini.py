d2b5ded8512fcf7213b4e86df5f2c7dc61675fb2
Eng-Moaz <156330236+Eng-Moaz@users.noreply.github.com>
2026-04-15 19:17:14 +0200

    Prompt init

--- cli/test_gemini.py
+++ cli/test_gemini.py
@@ -1,16 +1,16 @@
 import os
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GOOGLE_API_KEY")
 if not api_key:
     raise RuntimeError("GOOGLE_API_KEY environment variable not set")
-from google import genai
 
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn about RAG? Use one paragraph maximum."
+    model='gemini-2.5-flash', contents="You are a helpful assistant"
 )
 print(response.text)
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
